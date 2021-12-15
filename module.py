f0a13c42a0131cd5c4d66e31f1d1fe75d92a7c73
sophie091524 <66400432+sophie091524@users.noreply.github.com>
2021-12-15 12:52:12 +0800

    Update module.py

--- module.py
+++ module.py
@@ -41,26 +41,19 @@ class BLSTM_frame_sig_att(nn.Module):
     def forward(self, x, hl): #hl:(B,6)
         B, Freq, T = x.size()
         x = x.permute(0,2,1) #(B, 257, T_length)->(B, T_length, 257) 
-        #print(f'x:{x.size()}')
         hl = hl.unsqueeze(1) #hl:(B,1,6)
-        #print(f'hl:{hl.size()}')
         hl_repeat = hl.repeat(1,T,1)
-        #print(f'after repeat, hl:{hl_repeat.size()}')
         x_concate = torch.cat((x,hl_repeat), 2)
-        #print(f'concatenate:{x_concate.size()}')
         
         out, _ = self.blstm(x_concate) #(B,T, 2*hidden)
-        #print(f'before att:{out.size()}')
         out = self.dropout(self.act_fn(self.linear1(out))).transpose(0,1) #(T_length, B,  128) 
         hasqi, _ = self.hasqiAtt_layer(out,out,out)
         haspi, _ = self.haspiAtt_layer(out,out,out) 
-        hasqi, haspi = hasqi.transpose(0,1), haspi.transpose(0,1) #(B, T_length, 128), #(B, T_length, 128)  
+        hasqi, haspi = hasqi.transpose(0,1), haspi.transpose(0,1) #(B, T_length, 128)  
         hasqi, haspi = self.hasqiframe_score(hasqi), self.haspiframe_score(haspi) #(B, T_length, 1) 
         hasqi, haspi = self.sigmoid(hasqi), self.sigmoid(haspi) #pass a sigmoid
         hasqi_fram, haspi_fram = hasqi.permute(0,2,1), haspi.permute(0,2,1) #(B, 1, T_length) 
-        #print(f'before average:{out.size()}')
         hasqi_avg, haspi_avg = self.hasqiaverage_score(hasqi_fram), self.haspiaverage_score(haspi_fram)  #(B,1,1)
-        #print(f'after average:{avg_score.size()}')
         
         return hasqi_fram, haspi_fram, hasqi_avg.squeeze(1), haspi_avg.squeeze(1) #(B, 1, T_length) (B,1) 
        
